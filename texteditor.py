9c4b083b89a836316e0e118ed4fcec9734047c4e
florian dahlitz <f2dahlitz@freenet.de>
2017-02-17 21:32:10 +0100

    added font choicer

--- texteditor.py
+++ texteditor.py
@@ -12,6 +12,7 @@ elif "3.3" in v or "3.4" in v:
     from tkinter import filedialog
 
 
+# save-button functionality
 def save_as():
     global text
     t = text.get("1.0", "end-1c")
@@ -21,15 +22,39 @@ def save_as():
     file1.close()
 
 
+# "Helvetics"-font
+def font_helvetica():
+    global text
+    text.config(font="Helvetica")
+
+
+# "Courier"-font
+def font_courier():
+    global text
+    text.config(font="Courier")
+
+
 # setting up layout
 root = Tk("Text Editor")
 root.title("Text Editor")
 
+# adding text field
 text = Text(root)
 text.grid()
 
-button = Button(root, text="Save", command = save_as)
+# adding save-button
+button = Button(root, text="Save", command=save_as)
 button.grid()
+
+# adding font choicer
+font = Menubutton(root, text="Font")
+font.grid()
+font.menu = Menu(font, tearoff=0)
+font["menu"] = font.menu
+helvetica = IntVar()
+courier = IntVar()
+font.menu.add_checkbutton(label="Courier", variable=courier, command=font_courier)
+font.menu.add_checkbutton(label="Helvetica", variable=helvetica, command=font_helvetica)
 
 # run GUI
 root.mainloop()
